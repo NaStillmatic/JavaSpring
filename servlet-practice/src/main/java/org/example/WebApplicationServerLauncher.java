6bf2b729d3108f3047cc46c01a670f90c27f5dc1
Joe <nastillmatic@gmail.com>
2022-12-06 16:28:44 +0900

    Ch04. 웹애플리케이션의 이해
    - 프로젝트 기본 설정

---
+++ servlet-practice/src/main/java/org/example/WebApplicationServerLauncher.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class WebApplicationServerLauncher {
+    private static final Logger logger = LoggerFactory.getLogger(WebApplicationServerLauncher.class);
+
+    public static void main(String[] args) throws Exception {
+        // 내장 톰캣
+        String webappDirLocation = "webapp/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        logger.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
