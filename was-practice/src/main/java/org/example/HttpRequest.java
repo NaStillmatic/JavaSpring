a029b2648f4bc8f8ac00a305eeb5100de6212536
Joe <nastillmatic@gmail.com>
2022-12-06 14:21:47 +0900

    Ch04. 웹애플리케이션의 이해
    - 02. 계산기 웹 프로그램 구현하기 Step1

---
+++ was-practice/src/main/java/org/example/HttpRequest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class HttpRequest {
+    private final RequestLine requestLine;
+
+    public HttpRequest(BufferedReader br) throws IOException {
+        this.requestLine = new RequestLine(br.readLine());
+    }
+    public boolean isGetRequest() {
+        return requestLine.isGetRequest();
+    }
+    public boolean matchPath(String path) {
+        return requestLine.matchPath(path);
+    }
+    public QueryStrings getQueryStrings() {
+        return requestLine.getQueryStrings();
+    }
+}
