d081d17023f98aad030c66a5165077c805b1e107
Joe <nastillmatic@gmail.com>
2022-12-06 15:34:53 +0900

    Ch04. 웹애플리케이션의 이해
    - 02. 계산기 웹 프로그램 구현하기 Step3
    - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -8,11 +8,14 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -31,9 +34,9 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 /**
-                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                 * Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
                  */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute((new ClientRequestHandler(clientSocket)));
             }
         }
     }
