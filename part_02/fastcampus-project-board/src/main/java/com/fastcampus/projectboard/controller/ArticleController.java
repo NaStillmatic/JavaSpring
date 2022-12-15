a880298aa72453142aa47bca93c2e85cca0c84fb
Joe <nastillmatic@gmail.com>
2022-12-15 11:16:58 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.
    테스트를 실패한체 올리면 `gradle build`가 실패하면
    배포 자동화에 악영향을 미치는 경우가 있으므로,
    실패 테세트는 테스트 제외 처리하는 방향으로 정책을
    잡아보기로 했다.
    이에 기능 구현중인 유닛테스트 모두 `@Disabled` 처리.

---
+++ part_02/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.fastcampus.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *  /articles/search
+ *  /articles/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
