3ce389622df65ce60c2a7b3758ece17751f0bce6
Joe <nastillmatic@gmail.com>
2022-12-22 17:39:51 +0900

    #44 -  FormDataEncoderTest: Void.class 수정
    이 테스트는 사용하지 않는 빈이 아예 없는게 아니라
    테스트 대상인 `FormDataEncoder`,
    잭슨 `ObjectMapper`를 사용함
    `@SpringBootTest`를 사용한다면,
    이들을 `@Import`로 넣는 것보다 이 변경점과 같이
    더 알맞은 방법을 사용할 수 있음
    
    실제로 프로젝트에 적용해보니 에러가 발생하였고
    리펙토링 과정에 다루는 내용을 보고 먼저 적용 하였다.

--- part_02/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/util/FormDataEncoderTest.java
+++ part_02/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/util/FormDataEncoderTest.java
@@ -13,8 +13,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("테스트 도구 - Form 데이터 인코더")
-@Import({FormDataEncoder.class, ObjectMapper.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = Void.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        classes = {FormDataEncoder.class, ObjectMapper.class}
+)
 class FormDataEncoderTest {
 
     private final FormDataEncoder formDataEncoder;
@@ -41,6 +43,8 @@ class FormDataEncoderTest {
 
         // When
         String result = formDataEncoder.encode(obj);
+
+
 
         // Then
         assertThat(result).isEqualTo(
