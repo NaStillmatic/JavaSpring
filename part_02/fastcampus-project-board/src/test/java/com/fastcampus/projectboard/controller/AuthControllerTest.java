40b95ab8ef8e77862c06983a888e971d2e6caea8
Joe <nastillmatic@gmail.com>
2022-12-26 18:29:03 +0900

    #53 Import 어노테이션을 SecurityConfig -> TestSecurityConfig 클래스로 변경
    테스트 실패되는것을 알고있었지만 강의에서는 나중에 언급이 되는것으로 확인되었다. 
    미리 커밋함

--- part_02/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ part_02/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,6 +1,6 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
 public class AuthControllerTest {
 
