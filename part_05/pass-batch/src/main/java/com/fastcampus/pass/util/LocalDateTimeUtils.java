048adaa40a07e5077dfcaa613d09e7bf5e6366fa
Joe <nastillmatic@gmail.com>
2023-01-27 18:15:52 +0900

    part - 5 - Batch  통계 데이터 생성

--- part_05/pass-batch/src/main/java/com/fastcampus/pass/util/LocalDateTimeUtils.java
+++ part_05/pass-batch/src/main/java/com/fastcampus/pass/util/LocalDateTimeUtils.java
@@ -4,9 +4,12 @@ import io.micrometer.core.instrument.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 
 public class LocalDateTimeUtils {
     public static final DateTimeFormatter YYYY_MM_DD_HH_MM = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    public static final DateTimeFormatter YYYY_MM_DD = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public static String format(final LocalDateTime localDateTime) {
         return localDateTime.format(YYYY_MM_DD_HH_MM);
@@ -21,5 +24,9 @@ public class LocalDateTimeUtils {
             return null;
         }
         return LocalDateTime.parse(localDateTimeString, YYYY_MM_DD_HH_MM);
+    }
+
+    public static int getWeekOfYear(final LocalDateTime localDateTime) {
+        return localDateTime.get(WeekFields.of(Locale.KOREA).weekOfYear());
     }
 }
