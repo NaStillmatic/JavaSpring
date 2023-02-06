7bd6ec83e7c6a24b20cfca72fc1bd422a879f0d9
Joe <nastillmatic@gmail.com>
2023-02-06 17:06:19 +0900

    part - 6 - SpringMulti Model 프로젝트 생성

---
+++ part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -0,0 +1,17 @@
+package dev.be.moduleapi.service;
+
+import com.example.modulecommon.enums.CodeEnum;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String save() {
+        System.out.println(CodeEnum.SUCCESS.getCode());
+        return "save";
+    }
+
+    public String find() {
+        return "find";
+    }
+}
