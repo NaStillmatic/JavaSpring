900ce9a2b4a62674c1d9f8f94fea2d6a419b4c20
Joe <nastillmatic@gmail.com>
2023-02-06 17:24:52 +0900

    part - 6 - Srping Multi Module 의존성 설정 (3)
    - Bean 을 사용한 모듈간 의존성 테스트

--- part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,13 +1,18 @@
 package dev.be.moduleapi.service;
 
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
 
+    private final CommonDemoService commonDemoService;
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
+        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
