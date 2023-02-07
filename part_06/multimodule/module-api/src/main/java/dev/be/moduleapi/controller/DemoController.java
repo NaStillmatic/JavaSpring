fc1a6f7e75ce0fd9b2d83c6c75893bc29763a547
Joe <nastillmatic@gmail.com>
2023-02-07 18:06:27 +0900

    part - 6 - Srping Multi Module 의존성 설정 (6)
    멀티 모듈에서 DB 연동 테스트

--- part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
+++ part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -2,7 +2,9 @@ package dev.be.moduleapi.controller;
 
 import dev.be.moduleapi.exception.CustomException;
 import dev.be.moduleapi.service.DemoService;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repositories.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,20 @@ public class DemoController {
 
     private final DemoService demoService;
 
+    private final MemberRepository memberRepository;
+
     @GetMapping("/save")
     public String save() {
+        memberRepository.save(Member.builder()
+                .name(Thread.currentThread().getName())
+                .build());
         return demoService.save();
     }
 
     @GetMapping("/find")
     public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println("DB size: " + size);
         return demoService.find();
     }
 
---
+++ part_06/multimodule/module-common/src/main/java/dev/be/modulecommon/domain/Member.java
@@ -0,0 +1,24 @@
+package dev.be.modulecommon.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
