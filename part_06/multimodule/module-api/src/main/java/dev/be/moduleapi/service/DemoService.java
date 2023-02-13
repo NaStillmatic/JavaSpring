5701be5ab482f3e8da27a5cb7484b956d998bb6e
Joe <nastillmatic@gmail.com>
2023-02-13 11:43:40 +0900

    part - 6 - Profile 설정

--- part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ part_06/multimodule/module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -6,16 +6,22 @@ import dev.be.modulecommon.enums.CodeEnum;
 import dev.be.modulecommon.repositories.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DemoService {
 
+    @Value("${profile-name}")
+    private String name;
+
     private final CommonDemoService commonDemoService;
     private final MemberRepository memberRepository;
 
     public String save() {
+
+        System.out.println("name : " + name);
         System.out.println(CodeEnum.SUCCESS.getCode());
         System.out.println(commonDemoService.commonService());
         memberRepository.save(Member.builder()
