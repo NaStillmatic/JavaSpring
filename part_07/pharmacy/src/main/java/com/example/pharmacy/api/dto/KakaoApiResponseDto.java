9e60bd94d75cfc15a6fb49025950f3f3f99031ba
Joe <nastillmatic@gmail.com>
2023-02-16 16:32:10 +0900

    part - 7 - kakao 주소검색 api 구현하기

---
+++ part_07/pharmacy/src/main/java/com/example/pharmacy/api/dto/KakaoApiResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.pharmacy.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class KakaoApiResponseDto {
+
+    private MetaDto metaDto;
+
+    private List<DocumentDto> documentDtoList;
+}
