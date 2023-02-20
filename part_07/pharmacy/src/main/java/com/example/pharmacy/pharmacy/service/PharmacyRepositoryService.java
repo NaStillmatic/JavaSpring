bd7c89177339b1be1284e20fb11893fe0d3b3919
Joe <nastillmatic@gmail.com>
2023-02-20 17:47:38 +0900

    part - 7 - Spring Transactional 사용시 주의사항

--- part_07/pharmacy/src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRepositoryService.java
+++ part_07/pharmacy/src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRepositoryService.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -15,6 +17,22 @@ import java.util.Objects;
 public class PharmacyRepositoryService {
 
     private final PharmacyRepository pharmacyRepository;
+
+    // self invocation test
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error"); // 예외 발생
+        });
+    }
 
     @Transactional
     public void updateAddress(Long id, String address) {
--- part_07/pharmacy/src/test/groovy/com/example/pharmacy/pharmacy/service/PharmacyRepositoryServiceTest.groovy
+++ part_07/pharmacy/src/test/groovy/com/example/pharmacy/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -60,4 +60,29 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
         then:
         result.get(0).getPharmacyAddress() == address
     }
+
+    def "self invocation"() {
+
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepositoryService.bar(Arrays.asList(pharmacy))
+
+        then:
+        def e = thrown(RuntimeException.class)
+        def result = pharmacyRepositoryService.findAll()
+        result.size() == 1 // 트랜잭션이 적용되지 않는다( 롤백 적용 X )
+    }
+
 }
