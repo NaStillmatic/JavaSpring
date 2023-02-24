8fe98ac16fd537e79a68fcbdc1d95eb254cd0516
Joe <nastillmatic@gmail.com>
2023-02-24 18:14:18 +0900

    part - 7 - 잘못된 임포트 수정

--- part_07/pharmacy/src/main/java/com/example/pharmacy/api/service/KakaoCategorySearchService.java
+++ part_07/pharmacy/src/main/java/com/example/pharmacy/api/service/KakaoCategorySearchService.java
@@ -2,8 +2,8 @@ package com.example.pharmacy.api.service;
 
 import com.example.pharmacy.api.dto.KakaoApiResponseDto;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
