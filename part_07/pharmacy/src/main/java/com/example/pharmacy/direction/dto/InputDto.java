4158732e13100aa3db5b4c26f5e815d726e7e6e1
Joe <nastillmatic@gmail.com>
2023-02-27 14:21:27 +0900

    part - 7 - 메인화면, 결과화면 구현

---
+++ part_07/pharmacy/src/main/java/com/example/pharmacy/direction/dto/InputDto.java
@@ -0,0 +1,10 @@
+package com.example.pharmacy.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+    private String address;
+}
--- part_07/pharmacy/src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
+++ part_07/pharmacy/src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.example.pharmacy.pharmacy.service;
 import com.example.pharmacy.api.dto.DocumentDto;
 import com.example.pharmacy.api.dto.KakaoApiResponseDto;
 import com.example.pharmacy.api.service.KakaoAddressSearchService;
+import com.example.pharmacy.direction.dto.OutputDto;
 import com.example.pharmacy.direction.entity.Direction;
 import com.example.pharmacy.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,18 +24,36 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
-//        List<Direction> directionList = directionService.buildDirectionList(documentDto);
-        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
-        directionService.saveAll(directionList);
+
+        // 공공기관 약국 데이터 및 거리계산 알고리즘 이용
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
+
+        // kakao 카테고리를 이용한 장소 검색 api 이용
+//        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
+
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo") // todo
+                .roadViewUrl("todo") // todo
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
